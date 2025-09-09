512d478abbc2fcb782ce61ab48ce3fdfbe06e0be
Jaider Aprilla <jaiderreyes273@gmail.com>
2025-09-08 23:53:50 -0500

    Se completo el archivo almacenamiento.py

--- models/categorias/almacenamiento.py
+++ models/categorias/almacenamiento.py
@@ -1,3 +1,25 @@
 """
 Clase abstracta para muebles de almacenamiento.
 """
+from abc import ABC, abstractmethod
+from models.mueble import Mueble
+
+class Almacenamiento(Mueble, ABC):
+    def __init__(self, material: str, color: str, alto: float, ancho: float, profundo: float):
+        super().__init__(material, color, alto, ancho, profundo)
+        self._puertas = puertas
+        self._cajones = cajones
+    
+    @property
+    def puertas(self):
+        return self._puertas
+    @property
+    def cajones(self):
+        return self._cajones
+    @abstractmethod
+    def calcular_precio(self):
+        pass
+
+    @abstractmethod
+    def obtener_descripcion(self):
+        pass 
