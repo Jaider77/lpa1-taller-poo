671432445bcd492337f1cabbb5dbb01f75c46fa8
Jaider Aprilla <jaiderreyes273@gmail.com>
2025-09-10 19:15:45 -0500

    se compeltaron los archivos cama.py y sofa.py

--- models/concretos/cama.py
+++ models/concretos/cama.py
@@ -1,3 +1,39 @@
 """
 Clase concreta Cama.
 """
+from models.categorias.almacenamiento import Almacenamiento
+class Cama(Almacenamiento):
+       def __init__(self, nombre: str, material: str, color: str, precio_base: float,
+                 tamaño_cama: str = "matrimonial", incluye_colchon: bool = True):
+        # Puertas y cajones en cama suelen ser 0
+        super().__init__(nombre, material, color, precio_base, puertas=0, cajones=0)
+        self._tamaño_cama = tamaño_cama
+        self._incluye_colchon = incluye_colchon
+
+       @property
+       def tamaño_cama(self) -> str:
+          return self._tamaño_cama
+
+       @property
+       def incluye_colchon(self) -> bool:
+           return self._incluye_colchon
+       
+       def calcular_precio(self) -> float:
+              precio = self.precio_base
+              if self.tamaño_cama == "individual":
+                precio += 200
+              elif self.tamaño_cama == "matrimonial":
+                precio += 350
+              elif self.tamaño_cama == "queen":
+                precio += 300
+              elif self.tamaño_cama == "king":                      
+                precio += 400
+              if self.incluye_colchon:
+                precio += 250
+              return round(precio, 2)
+       def obtener_descripcion(self) -> str:
+          return (f"Cama '{self.nombre}' de tamaño {self.tamaño_cama}, material {self.material}, "
+                  f"color {self.color}, {'incluye colchón' if self.incluye_colchon else 'sin colchón'}, "
+                  f"precio: ${self.calcular_precio():.2f}")
+       
+     
--- models/concretos/sofa.py
+++ models/concretos/sofa.py
@@ -1,3 +1,23 @@
 """
 Clase concreta Sofa.
 """
+from models.categorias.asientos import Asiento
+
+class Sofa(Asiento):
+    def __init__(self, nombre: str, material: str, color: str, precio_base: float, capacidad_personas: int=3, tiene_respaldo: bool=True, material_tapizado: str="tela"):
+       super().__init__(nombre, material, color, precio_base, capacidad_personas, tiene_respaldo, material_tapizado)
+
+    def calcular_precio(self) -> float:
+        precio = self.precio_base
+        precio += self.calcular_factor_comodidad() * 30
+        if self.tiene_respaldo:
+            precio += 50
+        precio += self.capacidad_personas * 40
+        return round(precio, 2)
+    def obtener_descripcion(self)-> str:
+        return (f"sofá '{self.nombre}' de material {self.material}, color {self.color}, "
+                f"capacidad para {self.capacidad_personas} personas, "
+                f"tapizado en {self.material_tapizado if self.material_tapizado else 'sin tapizado'}, "
+                f"precio: ${self.calcular_precio():.2f}")
+
+       
